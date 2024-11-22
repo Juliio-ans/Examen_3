4cbc9ebce0633e0f7ef5fef2c3a8e798d8023a9e
Juliio-ans <juliodiaz9247gmail.com>
2024-11-22 12:12:31 -0600

    arreglo

--- avion_regional.py
+++ avion_regional.py
@@ -5,16 +5,16 @@ class AvionRegional(AvionComercial):
     def __init__(self):
         super().__init__()
         AvionComercial.__init__(self)
-        self._cantidad_asientos_clase_economica = 0
+        self._motorelices = 0
 
  # Getters y Setters
 
     def get_cantidad_asientos_clase_economica(self):
-            return self._cantidad_asientos_clase_economica
+            return self._motorelices
 
-    def set_cantidad_asientos_clase_economica(self, cantidad_asientos_clase_economica):
-            self._cantidad_asientos_clase_economica = cantidad_asientos_clase_economica
+    def set_cantidad_asientos_clase_economica(self, motorelices):
+            self._motorelices = motorelices
 
     def mostrar_info_regional(self):
         base_info = super().mostrar_info_comercial()
-        return f"{base_info} Asientos clase económica: {self._cantidad_asientos_clase_economica}"
+        return f"{base_info} No motores de elices: {self._motorelices}"
